8201d9681d22c5af9576078b5d3e3f8657549fde
camilatereza <camila.tereza@dcx.ufpb.br>
2022-05-18 18:05:40 -0300

    styles gerais

---
+++ src/global/styles/tabStyle.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+import { theme } from "./theme";
+
+export const Styles = StyleSheet.create({
+    container:{
+        width: '100%',
+        height: '8%',
+        backgroundColor: theme.colors.navigation,
+        borderRadius: 20,
+        marginTop:5
+    }
+})
---
+++ src/global/styles/theme.js
@@ -0,0 +1,13 @@
+export const theme = {
+    colors: {
+        backgroundColor: '#EBEBEB',
+        heading: '#EBEBEB',
+        button: '#3A32B7',
+        navigation: '#FFFFFF',
+        tabInactive: '#626A8D'
+    },
+    fonts:{
+        title: 'OverpassMono_700Bold',
+        text: 'OverpassMono_400Regular'
+    }
+}
